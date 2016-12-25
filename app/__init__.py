c97c30cb775e4afe01522b6e742853b8083a2a22
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-12-24 22:43:01 -0500

    Added Sentry support ᕙ(⇀‸↼‶)ᕗ

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 from flask_restplus import Api
 from flask_limiter import Limiter, util
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -11,5 +12,7 @@ app.config.from_object("config")
 api = Api(app)
 
 limiter = Limiter(app, key_func=util.get_remote_address)
+
+sentry = Sentry(app, dsn=app.config["SENTRY_DSN"])
 
 from . import views
