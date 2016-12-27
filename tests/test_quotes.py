eefe122c6e6df8751c8e83b0402582057fdbdcee
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-12-26 19:32:44 -0500

    Moved tests to a class, still not working.
    @2Cubed haaaaaalp

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -1,2 +1,31 @@
-def test_foo(client):
-    assert True
+import pytest
+
+
+# @pytest.mark.userfixtures("client")
+# class TestQuotes:
+#     creation_data = {"quote": "Cacti rock!"}
+#     url = "/api/v1/user/ParadigmShift3d/quote"
+#     data = {}
+#
+#     def test_create(self):
+#         # Valid user creation
+#         user = client.post(self.url, data=self.creation_data)
+#         self.data = user.json
+#         assert "data" in self.data
+#         assert "attributes" in self.data["data"]
+#         assert "id" in self.data["data"]
+#
+#     def test_single(self):
+#         user = client.get(self.url)
+#         user_single_data = user.json
+#         assert "data" in user_single_data
+#         assert "attributes" in user_single_data["data"]
+#         assert "id" in user_single_data["data"]
+#         assert user_single_data["data"]["id"] == self.data["data"]["id"]
+#
+#     def test_removal(self):
+#         user = client.delete(self.url)
+#         deletion_data = user.json
+#         assert "meta" in deletion_data
+#         assert "deleted" in deletion_data["meta"]
+#         assert deletion_data["meta"]["deleted"][0] == self.data["data"]["id"]
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,43 +1,36 @@
 import pytest
 
-user_creation_data = {
-    "service": "beam",
-    "userId": 24228,
-    "password": "foo",
-    "token": "paradigmshift3d"
-}
 
-
-def test_user(client):
+@pytest.mark.userfixtures("client")
+class TestUsers:
+    creation_data = {
+        "service": "beam",
+        "userId": 24228,
+        "password": "foo",
+        "token": "paradigmshift3d"
+    }
     url = "/api/v1/user/ParadigmShift3d"
-    user_data = user_creation(client, url)
-    user_single(client, user_data, url)
-    user_removal(client, user_data, url)
+    data = {}
 
+    def test_create(self, client):
+        # Valid user creation
+        user = client.post(self.url, data=self.creation_data)
+        self.data = user.json
+        assert "data" in self.data
+        assert "attributes" in self.data["data"]
+        assert "id" in self.data["data"]
 
-def user_creation(client, url):
-    # Valid user creation
-    user = client.post(url, data=user_creation_data)
-    user_data = user.json
-    assert "data" in user_data
-    assert "attributes" in user_data["data"]
-    assert "id" in user_data["data"]
+    def test_single(self, client):
+        user = client.get(self.url)
+        user_single_data = user.json
+        assert "data" in user_single_data
+        assert "attributes" in user_single_data["data"]
+        assert "id" in user_single_data["data"]
+        assert user_single_data["data"]["id"] == self.data["data"]["id"]
 
-    return user_data
-
-
-def user_single(client, user_data, url):
-    user = client.get(url)
-    user_single_data = user.json
-    assert "data" in user_single_data
-    assert "attributes" in user_single_data["data"]
-    assert "id" in user_single_data["data"]
-    assert user_single_data["data"]["id"] == user_data["data"]["id"]
-
-
-def user_removal(client, user_data, url):
-    user = client.delete(url)
-    deletion_data = user.json
-    assert "meta" in deletion_data
-    assert "deleted" in deletion_data["meta"]
-    assert deletion_data["meta"]["deleted"][0] == user_data["data"]["id"]
+    def test_removal(self, client):
+        user = client.delete(self.url)
+        deletion_data = user.json
+        assert "meta" in deletion_data
+        assert "deleted" in deletion_data["meta"]
+        assert deletion_data["meta"]["deleted"][0] == self.data["data"]["id"]
