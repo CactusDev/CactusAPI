6b469c7866c9975ed5a688ae2fc7784d695883bb
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-12-27 00:40:19 -0500

    Integration tests suuuuuck

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,17 +6,22 @@ import rethinkdb as rethink
 sys.path.append(".")
 
 
+def pytest_sessionfinish(session):
+    r = rethink.connect(db="testing")
+    for key in rethink.db("testing").table_list().run(r):
+        rethink.db("testing").table(key).delete().run(r)
+
+
 @pytest.fixture
 def app(request):
     from app import app
     app.config["TESTING"] = True
     app.config["RDB_DB"] = "testing"
-    r = rethink.connect(db="testing")
-
-    def tearDown():
-        for table in rethink.db("testing").table_list().run(r):
-            rethink.db("testing").table(table).delete().run(r)
-
-    request.addfinalizer(tearDown)
 
     return app
+
+
+@pytest.fixture
+def client(app):
+    with app.test_client() as client:
+        yield client
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,7 @@
 import pytest
+import rethinkdb as rethink
 
 
-@pytest.mark.userfixtures("client")
 class TestUsers:
     creation_data = {
         "service": "beam",
@@ -13,24 +13,23 @@ class TestUsers:
     data = {}
 
     def test_create(self, client):
-        # Valid user creation
+        """Valid user creation"""
         user = client.post(self.url, data=self.creation_data)
-        self.data = user.json
-        assert "data" in self.data
+        for key, value in user.json.items():
+            self.data[key] = value
         assert "attributes" in self.data["data"]
         assert "id" in self.data["data"]
 
     def test_single(self, client):
+        """Get a single user object and see if it matches (it should)"""
         user = client.get(self.url)
         user_single_data = user.json
-        assert "data" in user_single_data
-        assert "attributes" in user_single_data["data"]
         assert "id" in user_single_data["data"]
         assert user_single_data["data"]["id"] == self.data["data"]["id"]
 
     def test_removal(self, client):
+        """Remove a user and see if it matches"""
         user = client.delete(self.url)
         deletion_data = user.json
-        assert "meta" in deletion_data
-        assert "deleted" in deletion_data["meta"]
+
         assert deletion_data["meta"]["deleted"][0] == self.data["data"]["id"]
