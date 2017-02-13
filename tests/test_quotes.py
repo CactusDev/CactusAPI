744abc3cbf054003db1a33e7fccce66bf0669ec3
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-02-12 23:14:38 -0500

    Beginning to implement tests again

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -1,45 +1,70 @@
 import pytest
+import json
 
 
 class TestQuotes:
     creation_data = [{"quote": "Cacti rock!"}, {"quote": "CactusBot!"}]
     url = "/api/v1/user/paradigmshift3d/quote"
-    data = {}
 
     def test_create(self, client):
         """Valid quote creation"""
         quote = client.post(self.url, data=self.creation_data[0])
-        self.data.update(quote.json)
 
-        assert "attributes" in self.data["data"]
-        assert "id" in self.data["data"]
-        assert self.data["data"]["attributes"]["quoteId"] == 1
-        assert self.data["data"]["attributes"][
+        if hasattr(quote, "json"):
+            update_data = quote.json
+        else:
+            update_data = json.loads(quote.data.decode('utf-8'))
+
+        assert "data" in update_data
+        assert "attributes" in update_data["data"]
+        assert "id" in update_data["data"]
+        assert update_data["data"]["attributes"]["quoteId"] == 1
+        assert update_data["data"]["attributes"][
             "quote"] == self.creation_data[0]["quote"]
 
     def test_single(self, client):
         """Get a single quote object and see if it matches (it should)"""
+
+        quote = client.post(self.url, data=self.creation_data[0])
+
+        if hasattr(quote, "json"):
+            data = quote.json
+        else:
+            data = json.loads(quote.data.decode('utf-8'))
+
         quote = client.get(
-            self.url + "/" + str(self.data["data"]["attributes"]["quoteId"]))
-        quote_single_data = quote.json
+            self.url + "/" + str(data["data"]["attributes"]["quoteId"]))
+
+        if hasattr(quote, "json"):
+            quote_single_data = quote.json
+        else:
+            quote_single_data = json.loads(quote.data.decode('utf-8'))
 
         assert quote_single_data["data"]["attributes"]["quoteId"] == 1
         assert quote_single_data["data"]["attributes"][
             "quote"] == self.creation_data[0]["quote"]
-        assert quote_single_data["data"]["id"] == self.data["data"]["id"]
+        assert quote_single_data["data"]["id"] == data["data"]["id"]
 
     def test_all(self, client):
         """Retrieve all quotes"""
-        # Create the second quote
-        quote = client.post(self.url, data=self.creation_data[1])
-        quote_create_data = quote.json
+        for to_create in self.creation_data:
+            index = self.creation_data.index(to_create)
+            quote = client.post(self.url, data=to_create)
+            if hasattr(quote, "json"):
+                quote_create_data = quote.json
+            else:
+                quote_create_data = json.loads(quote.data.decode('utf-8'))
 
-        assert quote_create_data["data"]["attributes"]["quoteId"] == 2
-        assert quote_create_data["data"]["attributes"][
-            "quote"] == self.creation_data[1]["quote"]
+            assert quote_create_data["data"][
+                "attributes"]["quoteId"] == index + 1
+            assert quote_create_data["data"]["attributes"][
+                "quote"] == self.creation_data[index]["quote"]
 
         quote = client.get(self.url)
-        quote_all_data = quote.json
+        if hasattr(quote, "json"):
+            quote_all_data = quote.json
+        else:
+            quote_all_data = json.loads(quote.data.decode('utf-8'))
         assert len(quote_all_data["data"]) == 2
 
     def test_random(self, client):
@@ -47,7 +72,11 @@ class TestQuotes:
         quote = client.get(
             self.url + "/" + str(self.data["data"]["attributes"]["quoteId"]),
             data={"random": "true"}
-        ).json
+        )
+        if hasattr(quote, "json"):
+            quote = quote.json
+        else:
+            quote = json.loads(quote.data.decode('utf-8'))
 
         i = 0
         while i < 10:
@@ -62,12 +91,12 @@ class TestQuotes:
         if i == 10:
             raise AssertionError(
                 "No random results returned within attempt limit")
-
-    def test_removal(self, client):
-        """Remove a user and see if it matches"""
-        # Using the quote ID from the first created quote
-        quote = client.delete(
-            self.url + "/" + str(self.data["data"]["attributes"]["quoteId"]))
-        deletion_data = quote.json
-
-        assert deletion_data["meta"]["deleted"][0] == self.data["data"]["id"]
+    #
+    # def test_removal(self, client):
+    #     """Remove a user and see if it matches"""
+    #     # Using the quote ID from the first created quote
+    #     quote = client.delete(
+    #         self.url + "/" + str(self.data["data"]["attributes"]["quoteId"]))
+    #     deletion_data = quote.json
+    #
+    #     assert deletion_data["meta"]["deleted"][0] == self.data["data"]["id"]
