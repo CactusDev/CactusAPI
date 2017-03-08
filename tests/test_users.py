b9669a6edc6717570a155c5fe4e1b68e744fb621
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-03-08 16:02:59 -0500

    User unittests are complete!

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,33 +1,56 @@
 import pytest
+import json
 
 
-# class TestUsers:
-#     creation_data = {
-#         "service": "beam",
-#         "userId": 24228,
-#         "password": "foo",
-#         "token": "paradigmshift3d"
-#     }
-#     url = "/api/v1/user/ParadigmShift3d"
-#     data = {}
-#
-#     def test_create(self, client):
-#         """Valid user creation"""
-#         user = client.post(self.url, data=self.creation_data)
-#         self.data.update(user.json)
-#         assert "attributes" in self.data["data"]
-#         assert "id" in self.data["data"]
-#
-#     def test_single(self, client):
-#         """Get a single user object and see if it matches (it should)"""
-#         user = client.get(self.url)
-#         user_single_data = user.json
-#         assert "id" in user_single_data["data"]
-#         assert user_single_data["data"]["id"] == self.data["data"]["id"]
-#
-#     def test_removal(self, client):
-#         """Remove a user and see if it matches"""
-#         user = client.delete(self.url)
-#         deletion_data = user.json
-#
-#         assert deletion_data["meta"]["deleted"][0] == self.data["data"]["id"]
+class TestUsers:
+    creation_data = {
+        "service": "beam",
+        "userId": 24228,
+        "password": "foo",
+        "token": "paradigmshift3d"
+    }
+    url = "/api/v1/user/ParadigmShift3d"
+
+    def test_create(self, client, api_auth):
+        """Valid user creation"""
+        user = client.post(self.url, data=self.creation_data, headers=api_auth)
+        data = json.loads(user.data.decode())["data"]
+
+        assert data["type"] == "user"
+        comparison_data = {
+            **{k: v for k, v in self.creation_data.items() if k != "password"},
+            "userName": "ParadigmShift3d"
+        }
+        assert data["attributes"] == comparison_data
+
+        # Delete the user so we have a clean environment to work in
+        client.delete(self.url, headers=api_auth)
+
+    def test_single(self, client, api_auth):
+        """Get a single user object and see if it matches (it should)"""
+        user = client.post(self.url, data=self.creation_data, headers=api_auth)
+        creation_data = json.loads(user.data.decode())["data"]
+
+        comparison_data = {
+            **{k: v for k, v in self.creation_data.items() if k != "password"},
+            "userName": "ParadigmShift3d"
+        }
+
+        user = client.get(self.url)
+        single_data = json.loads(user.data.decode())["data"]
+        assert single_data["id"] == creation_data["id"]
+        assert single_data["attributes"] == comparison_data
+
+        # Clean up the DB
+        client.delete(self.url, headers=api_auth)
+
+    def test_removal(self, client, api_auth):
+        """Remove a user and see if it matches"""
+        user = client.post(
+            self.url, data=self.creation_data, headers=api_auth)
+        data = json.loads(user.data.decode())["data"]
+
+        user = client.delete(self.url, headers=api_auth)
+        deletion_data = json.loads(user.data.decode())
+        assert len(deletion_data["meta"]["deleted"]) == 1
+        assert deletion_data["meta"]["deleted"][0] == data["id"]
