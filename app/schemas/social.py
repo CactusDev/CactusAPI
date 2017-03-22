647622529ee087088add9283c766088caf23d1f8
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-03-21 21:30:30 -0400

    Pushing to develop :O

--- app/schemas/social.py
+++ app/schemas/social.py
@@ -6,6 +6,6 @@ class SocialSchema(Schema):
     id = fields.String()
     token = fields.String(required=True)
     service = fields.String(required=True)
-    url = fields.Url(required=True)
+    url = fields.String(required=True)
     createdAt = fields.DateTime(
         "%c", default=datetime.utcnow().strftime("%c"), dump_only=True)
